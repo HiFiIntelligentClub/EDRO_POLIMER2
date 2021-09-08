ed9c0a199d81e9d46f7076c81a36a658896640c3
Andrey Chekmarev <assminog@gmail.com>
2021-09-08 16:51:49 +0300

    stepx1

---
+++ 001.Устройства/000.Ошибка.php
@@ -0,0 +1,22 @@
+<?php
+// © A.A.CheckMaRev assminog@gmail.com tubmulur@yandex.ru 2021
+class ОповещениеОшибка
+	{
+	private $E	= array(
+			
+			);
+	private $D	= array(
+			
+			);
+	private $R	= array(
+			
+			);
+	public  $O	= array(
+			
+			);
+	public function __construct($м=array())
+		{
+		
+		}
+	}
+?>
