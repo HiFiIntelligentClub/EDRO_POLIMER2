e8b49a0470f088af6790b0890893d40a3cde8f90
Andrey Chekmarev <assminog@gmail.com>
2021-09-11 21:25:16 +0300

    Add files via upload

---
+++ СерверЧтение1/002.Оповещение/001.Состояние.php
@@ -0,0 +1,27 @@
+<?php
+// © A.A.CheckMaRev assminog@gmail.com tubmulur@yandex.ru 2021
+class ОповещениеСостояние
+	{
+	private $E	= array(
+			
+			);
+	private $D	= array(
+			
+			);
+	private $R	= array(
+			
+			);
+	public  $O	= array(
+			
+			);
+	public function __construct($о=array())
+		{
+		//print_r($о);
+		}
+	public function  _PushCondition($о=array())
+		{
+		echo __CLASS__."\n";
+		print_r($о);
+		}
+	}
+?>
