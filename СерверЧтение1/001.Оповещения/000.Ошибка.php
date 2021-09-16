dc369daa06120b1f7633cdb6cc8707b9bef08298
Andrey Chekmarev <assminog@gmail.com>
2021-09-17 01:45:00 +0300

    Add files via upload

---
+++ СерверЧтение1/001.Оповещения/000.Ошибка.php
@@ -0,0 +1,27 @@
+<?php
+// © A.A.CheckMaRev assminog@gmail.com tubmulur@yandex.ru 2021
+class ОповещениеОшибка
+	{
+	private $E	= array(
+			
+			);
+	private $D	= array(
+			
+			);
+	private $R	= array(
+			
+			);
+	public  $O	= array(
+			
+			);
+	public function __construct($о=array())
+		{
+		///print_r($м);
+		}
+	public function _PushError($о)
+		{
+		echo __CLASS__."\n";
+		print_r($о);
+		}
+	}
+?>
