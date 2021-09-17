db144f1a129742ae3c3c36e9726cc43f6bbd1a15
Andrey Chekmarev <assminog@gmail.com>
2021-09-17 04:06:38 +0300

    Add files via upload

--- СерверЧтение1/001.Оповещения/001.Состояние.php
+++ СерверЧтение1/001.Оповещения/001.Состояние.php
@@ -14,14 +14,39 @@ class ОповещениеСостояние
 	public  $O	= array(
 			
 			);
+	private function _VoidAuthorAndSystemName()
+		{
+		system('clear');
+		echo '-------------------------------------------------------------------------------------------------------------------------------------------'."\n";
+		echo '----------=ЕДРО:ПОЛИМЕР II (Только чтение) © A.A.CheckMaRev assminog@gmail.com, tubmulur@yandex.ru, hfic.samin@vk.com 2021=----------------'."\n";
+		echo '-------------------------------------------------------------------------------------------------------------------------------------------'."\n";
+		echo "\n";
+		echo "\n";
+		}
 	public function __construct($о=array())
 		{
 		//print_r($о);
 		}
 	public function  _PushCondition($о=array())
 		{
-		echo __CLASS__."\n";
-		//print_r($о);
+		$this->_VoidAuthorAndSystemName();
+		//echo __CLASS__."\n";
+		if(isset($о->O))
+			{
+			//print_r($о->O['оСекундомер']);
+			echo 'Текущ: '.$о->O['оСекундомер']->sClass.'/'.$о->O['оСекундомер']->sFuncion.': '.$о->O['оСекундомер']->д4Итого."\n";
+			if(is_array($о->O['оСекундомер']->мОценка))
+				{
+				foreach($о->O['оСекундомер']->мОценка as $ч0 => $м)
+					{
+					if(is_array($м))
+						{
+						echo 'Превыш: '.$м['сМетод'].': '.$м['д4Итого']."\n";
+						}
+					
+					}
+				}
+			}
 		}
 	}
 ?>
