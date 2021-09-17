6b71cc7cc308fcc6caaa80c6c3c2dae11f6858a4
Andrey Chekmarev <assminog@gmail.com>
2021-09-17 04:07:29 +0300

    Add files via upload

--- СерверЧтение1/003.РаботаСЧасами/004.Секундомер.php
+++ СерверЧтение1/003.РаботаСЧасами/004.Секундомер.php
@@ -1,18 +1,17 @@
 <?php
-//© A.A.CheckMaRev assminog@gmail.com tubmulur@yandex.ru
-//$оСекундомер = new Секундомер::о();
-//$оСекундомер->_Стоп();
-//print_r($оСекундомер);
+//© A.A.CheckMaRev assminog@gmail.com tubmulur@yandex.ru Hfic.Samin@vk.com 2021
+//$this->O['оСекундомер'] 		= new Секундомер(__CLASS__, __FUNCTION__);
+//					$this->O['оСекундомер']->_Старт(__CLASS__, __FUNCTION__);
+//					$this->O['оСекундомер']->_Стоп(__CLASS__, __FUNCTION__);
 class Секундомер
 	{
 	private  $м		= array();
 	private  $д4Старт	= 0.00;
 	private  $д4Стоп	= 0.00;
 	public   $д4Итого 	= 0.00;
-	public   $оOценка;											// o20
-	private  $sClass	= '';
-	private  $sFuncion	= '';
-
+	public  $sClass		= '';
+	public  $sFuncion	= '';
+	public   $мОценка	= array();											// o20
 	public function __construct($sClass, $sFuncion)                                            		//	><	//  .  .      //////// /
 		{       
 		$this->sClass	= $sClass;                                                      		//	Роскосмос . >< .
@@ -20,40 +19,35 @@ class Секундомер
 		//switch($сЕсли, $сОценкаДля= ОблакоРегРу480рВМес)                                          /////   ......    //     ////
 		//	{                                                              		 //////////    / .      .  /         /
 			                                                               		////////....../ .        . /////   / 
-		$this->м[$sClass][$sFuncion]['д4Вкл']	= д4Время();                                        //                       .      .
+		$this->м[$sClass]['д4Вкл']	= д4Время();                                        //                       .      .
 			        					//	              //           ....
-			
-		//	case 'Стоп':
-		//		$this->_Стоп();
-		//	break;
-		//	$this->оOценка();								//////
-		//	}									//	//
 		}											/////////
 	public function _Старт($sClass, $sFuncion)							//
 		{											// new Скорость
 		$this->м[$sClass][$sFuncion]['д4Старт']	= д4Время();
+		$д4Старт				= $this->м[$sClass]['д4Вкл'];
+		$д4Стоп					= $this->м[$sClass][$sFuncion]['д4Старт'];
+		$this->д4Итого				= round($д4Стоп	 - $д4Старт, 4);
+							$this->_Оценка();
 		}
 	public function _Стоп($sClass, $sFuncion)
 		{
 		$this->м[$sClass][$sFuncion]['д4Стоп']	= д4Время();
-		$д4Стоп					= $this->м[$sClass][$sFuncion]['д4Стоп'];
 		$д4Старт				= $this->м[$sClass][$sFuncion]['д4Старт'];
+		$д4Стоп					= $this->м[$sClass][$sFuncion]['д4Стоп'];
 		$this->д4Итого				= round($д4Стоп - $д4Старт, 4);
 							$this->_Оценка();
 		///print_r($this);
 		}
 	private function _Оценка()
 		{
-		//$this->оOценка				= new Oценка($this->д4Старт);
-		//  Значения производительности для хостинга и тарифа, для оценки производительности на платформе, до того, как всё повисло.
+		if($this->д4Итого>0.01)
+			{
+			$this->мОценка[]		= array(
+							'сМетод'	=> $this->sClass.'/'.$this->sFuncion,
+							'д4Итого'	=> $this->д4Итого
+							);
+			}
 		}
-	//public static function д4()
-	//	{
-		//return new Секундомер();
-	//	}
-	//public static function о()
-	//	{
-	//	return new Секундомер();
-	//	}
 	}
 ?>
