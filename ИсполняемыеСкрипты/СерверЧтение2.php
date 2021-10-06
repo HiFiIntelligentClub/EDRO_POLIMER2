5b69c29cd53efb2a508771a70c086410fcd73376
Andrey Chekmarev <assminog@gmail.com>
2021-10-06 14:58:45 +0300

    Step 2 of 4

--- ИсполняемыеСкрипты/СерверЧтение2.php
+++ ИсполняемыеСкрипты/СерверЧтение2.php
@@ -1844,6 +1844,154 @@ function strMyJson($_arrJson)
 	{
 	return substr(strMyJsonRec($_arrJson),0,-2);
 	}
+
+		          /*© A.A.CheckMaRev assminog@gmail.com*/
+		    ////// 				//
+		   //   /\ RCe			/////////
+		      //  <  **> 				//
+		     //     Jl   				//
+		    //////				/////////
+		    //$_arrData=array('strDir'=>'dir', 'strFile'=>'file');
+class MyXML
+	{
+	private $bTagIn		=false;
+	private $int0TagOpen	=0;
+	private $int0TagClose	=0;
+	private	$strInTag	='';
+	private	$strTagOpen	='';
+	public $arrTag		=array();
+	public function __construct($strXML, $strTagOpen)
+		{
+
+		$strXML			=(string)$strXML;
+		$arrOpen['str']		=(string)'<'.$strTagOpen.'>';
+		$arrOpen['len']		=strlen($arrOpen['str']);
+		$arrClose['str']	=(string)'</'.$strTagOpen.'>';
+		$arrClose['len']	=strlen($arrClose['str']);
+		$strIn			='';
+		$int1Length=strlen($strXML);
+		$bTagIn		=false;
+		$bTagOut	=false;
+		$intTag		=0;
+		$arrTag		=array();
+		for($int0I=0;$int0I<$int1Length;$int0I++)
+			{
+			//echo $intI;
+			if($strXML[$int0I]==$arrOpen['str'][$this->int0TagOpen])
+				{
+				$this->int0TagOpen++;
+				}
+			else
+				{
+				$this->int0TagOpen=0;
+				}
+			if($strXML[$int0I]==$arrClose['str'][$this->int0TagClose])
+				{
+				$this->int0TagClose++;
+				}
+			else
+				{
+				$this->int0TagClose=0;
+				}
+			if($this->int0TagOpen==$arrOpen['len'])
+				{
+				$this->int0TagOpen	=0;
+				$bTagIn			=true;
+				$bTagOut		=false;
+				}
+			if($this->int0TagClose==$arrClose['len'])
+				{
+				$strTagString					=substr($strIn,1,(-$arrClose['len']+1));
+				if($strTagOpen=='entry')
+					{
+					}
+				else
+					{
+					$arrTag[$strTagOpen]['str']			=$strTagString;
+					}
+				//$arrTag[$strTagOpen]['str']			=substr($strIn,1,(-$arrClose['len']+1));
+				$arrTags					=$this->arrTagName($strTagString);
+				if(!empty($arrTags))
+					{
+					foreach($arrTags as $strTag)
+						{
+						$arrTag[$strTagOpen][$strTag]	=MyXML::arr($strTagString, $strTag)[0]['str'];
+						}
+					}
+				//$arrTag[$strTagOpen]['server_name']		=Tag::arr($arrTag[$strTagOpen]['str'], 'server_name');
+				//$arrTag[$strTagOpen]['server_type']		=Tag::arr($arrTag[$strTagOpen]['str'], 'server_type');
+				//$arrTag[$strTagOpen][$intTag]['bitrate']	=Tag::arr($arrTag[$strTagOpen][$intTag]['str'], 'bitrate');
+				//$arrTag[$strTagOpen][$intTag]['samplerate']	=Tag::arr($arrTag[$strTagOpen][$intTag]['str'], 'samplerate');
+				//$arrTag[$strTagOpen][$intTag]['channels']	=Tag::arr($arrTag[$strTagOpen][$intTag]['str'], 'channels');
+				//$arrTag[$strTagOpen][$intTag]['listen_url']	=Tag::arr($arrTag[$strTagOpen][$intTag]['str'], 'listen_url');
+				//$arrTag[$strTagOpen][$intTag]['current_song']	=Tag::arr($arrTag[$strTagOpen][$intTag]['str'], 'current_song');
+				//$arrTag[$strTagOpen][$intTag]['genre']		=Tag::arr($arrTag[$strTagOpen][$intTag]['str'], 'genre');
+				$this->arrTag[]					=$arrTag[$strTagOpen];
+				$intTag++;
+				$strIn			='';
+				$this->int0TagClose	=0;
+				$bTagIn			=false;
+				$bTagOut		=true;
+				}
+			if($bTagIn)
+				{
+				$strIn	.=$strXML[$int0I];
+				}
+			else
+				{
+				}
+			//echo $strIn;
+			}
+		}
+	private function arrTagName($strXML)
+		{
+		$arr			=array();
+		$strArrType		='';
+		$int0Arr		=0;
+		$int0ArrLength		=0;
+		$strArr			='';
+		$strXML			=(string)$strXML;
+		$int1Length		=strlen($strXML);
+		$bTagIn			=false;
+		$bTagOut		=false;
+		for($int0I=0;$int0I<$int1Length;$int0I++)
+			{
+			if($strXML[$int0I]=='<')
+				{
+				$bTagIn		=true;
+				}
+			if(($strXML[$int0I]=='>'||$strXML[$int0I]==' '||$strXML[$int0I]=="\n")&&$bTagIn)
+				{
+				if($strArr[1]=='/')
+					{
+					}
+				else
+					{
+					$arr[$int0Arr]	=substr($strArr,1,$int0ArrLength);
+					}
+				$strArr		='';
+				$int0ArrLength	=0;
+				$int0Arr++;
+				$bTagIn		=false;
+				}
+			if($bTagIn)
+				{
+				//if($strXML[$int0I]=='/')
+				//	{
+				//	$strArrType	='Open';
+				//	}
+				$strArr		.=$strXML[$int0I];
+				$int0ArrLength++;
+				}
+			}
+		return 		$arr;
+		}
+	public static function arr($strXML, $strTagOpen)
+		{
+		$objTag	= new MyXML($strXML, $strTagOpen);
+		return $objTag->arrTag;
+		}
+	}
  
 //© A.A.CheckMaRev assminog@gmail.com tubmulur@yandex.ru Hfic.Samin@vk.com
 function д4Время()
@@ -1914,20 +2062,23 @@ class ЕДРО
 		{
 		switch($сД)
 			{
-			case'Прочитать':
+			case'Настройки':
 				$this->_ПрочитатьD($сРасположение);
-
+			break;
+			case'Объект':
+				$this->_ПрочитатьO($сРасположение);
 			break;
 			}
 		}
 	private function _ПрочитатьD($сРасположение)
 		{
-		$this->Р	= FileRead::objJSON($сРасположение.'.EDRO');
+		$this->Р	= FileRead::objJSON($сРасположение.'/run.D');
 		}
-	private function сПрочитатьO($сРасположение)
+	private function _ПрочитатьO($сРасположение)
 		{
 		//if(is_file($сРасположение.'/0.php'))
 		//	{
+			$this->О	= FileRead::objJSON($сРасположение.'/run.php');
 		//	}
 		//echo $сРасположение;
 		}
@@ -1984,8 +2135,10 @@ class EDRO
 		$this->R			= $oEvent->R;
 					    unset($oEvent);
 		$oDesign			= new Design($this->E);
-		///print_r($this);
-		//exit;
+		$this->D			= $oDesign->D;
+					    unset($oDesign);
+		print_r($this);
+		exit;
 		//$arr			= arrEventParams2Array($_сЗаголовки);
 		/*if($this->R['bizReadedBlock']===TRUE)
 			{
@@ -2061,11 +2214,11 @@ class Event
 	public $D	= array( //НастройкиЭлементаНадКоторымПроизводитсяРабота([D]esign - the screen in front of listener's eyes)
 			///	'дбг_сНастройкиЭлемента'=> '',
 			//	'мНастройкиЭлемента'	=> array(),
-			'arrHaveLanguage'		=> array(),
-			'arrHaveContentType'		=> array(),
-			'arrHaveEncoding'		=> array(),
+			'strLanguage'			=> '',
+			'strContentType'		=> '',
+			'strEncoding'			=> '',
 			'arrAllowedAccess'		=> array(),
-			'strAllowedConnection'		=> array(),
+			// 'strAllowedConnection'		=> array(),
 			
 			);
 	public $R	= array( //Состояние операционной среды Реальность ([R]eality)
@@ -2666,14 +2819,14 @@ class Design
 			//	'дбг_сВходящиеНастройки' => '',
 			//	'мВходящиеНастройки'	=> array(),
 			);
-	private $D	= array( //НастройкиЭлементаНадКоторымПроизводитсяРабота([D]esign - the screen in front of listener's eyes)
+	public $D	= array( //НастройкиЭлементаНадКоторымПроизводитсяРабота([D]esign - the screen in front of listener's eyes)
 			//	'дбг_сНастройкиЭлемента'=> '',
 			//	'мНастройкиЭлемента'	=> array(),
 			'arrHaveLanguage'		=> array(),
 			'arrHaveContentType'		=> array(),
 			'arrHaveEncoding'		=> array(),
 			'arrAllowedAccess'		=> array(),
-			'strAllowedConnection'		=> array(),
+			// 'strAllowedConnection'		=> array(),
 			);
 	private $R	= array( //Состояние операционной среды Реальность ([R]eality)
 			//	'сЗаголовки'		=> '',
@@ -2692,9 +2845,10 @@ class Design
 		$this->O['оСостояние'] 			= new ОповещениеСостояние();
 		$this->O['оСекундомер'] 		= new Секундомер(__CLASS__, __FUNCTION__);
 		$strTemplate				= сРасположениеО2оDB.сНазваниеО2оDB.'/Events/'.сПреобразовать($arrE['strName'], "вКоманду");
-		$this->O['оЕДРО'] 			= new ЕДРО('Прочитать', $strTemplate);
-		print_r($this);
-		exit;
+		$оЕДРО 					= new ЕДРО('Настройки', $strTemplate);
+		$this->D				= $оЕДРО->Р;
+		//print_r($this);
+		//exit;
 		}
 	public static function strObjectInit()
 		{
